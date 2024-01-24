f51f38aa186009a3ba1cb9458b3d394a832179ea
SangeethaGnanavel <sangeetha.gnana@gmail.com>
2024-01-24 16:00:34 -0500

    added api routes and mongoose methods for CRUD operations

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,23 @@
 const router = require("express").Router();
+const {
+  getThoughts,
+  createThoughts,
+  getSingleThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/courses
+router.route("/").get(getThoughts).post(createThoughts);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions/").post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,20 @@
 const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
+
+// /api/courses
+router.route("/").get(getUsers).post(createUser);
+
+// /api/courses/:courseId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
 module.exports = router;
